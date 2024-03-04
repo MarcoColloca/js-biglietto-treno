de677a902384168d14d613f4421489fa80ce52a1
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-04 15:41:17 +0100

    Add → added all exercise passages (after the exercised was done)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 console.log("JS Trial")
 
-
-
+//// Passi da eseguire
+/// Creare una costante che corrisponda ad un Prompt in cui venga richiesta la distanza percorsa al passeggero.
+/// Creare una costante che corrisponda ad un Prompt in cui venga richiesta l'età al passeggero.
+/// Definire una costante con il prezzo per km percorso.
+/// Definire una costante con il prezzo totale in base ai dati in possesso.
+/// Creare le variabili del biglietto in base all'età seguendo 3 passaggi:
+///   1• Se l'età è minore di 18 ridurre il prezzo all' 80%.
+///   2• Se l'età è superiore a 65 ridurre il prezzo al 60%.
+///   3• Se l'età non corrisponde alle variabili su scritte, ripotrare un prezzo normale.
+/// Stampare i prezzi per ogni variabile.
+/// Fare in modo che i prezzi stampati non superino le due cifre decimali. 
 
 
 const distance = parseFloat(prompt("Inserisci i km che vuoi percorrere (solo il numero)")); //Number
