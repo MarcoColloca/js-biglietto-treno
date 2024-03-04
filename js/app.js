b6b88a00b869cbb829815d55eb4d689b64aab002
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-04 14:58:53 +0100

    Add → added and verified prompt for Kms and Age in JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 console.log("JS Trial")
 
 
+//// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+//// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+//// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//// va applicato uno sconto del 20% per i minorenni
+//// oppure applicato uno sconto del 40% per gli over 65.
+//// L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.
+
+
+const km = parseFloat(prompt("Inserisci i km che vuoi percorrere")); //Number
+const age = parseFloat(prompt("Inserisci la tua età")); //Number
+
+console.log(km)
+console.log(age)
